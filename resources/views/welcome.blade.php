6d00f2bf997594c6dab60a589e91a1c1d3155be4
EricLow525 <happyeric525@gmail.com>
2017-02-03 15:42:12 +0000

    new design(dashboard)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,11 +8,6 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js">
     <title>insights.pics</title>
-    <link rel="stylesheet" href="/css/user.css">
-    <link rel="stylesheet" href="/css/Article-Clean.css">
-    <link rel="stylesheet" href="/css/Login-Form-Clean.css">
-    <link rel="stylesheet" href="/css/Registration-Form-with-Photo.css">
-    <link rel="stylesheet" href="/css/Team-Grid.css">
 </head>
 
 <body>
